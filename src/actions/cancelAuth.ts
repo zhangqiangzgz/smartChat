433977b5c84c5a8a0a0729e6d64a633184564512
zhangqiangzgz <zhangqiangzgz@hotmail.com>
2023-06-02 21:21:28 +0800

    refactor: cancelAuth

--- src/actions/cancelAuth.ts
+++ src/actions/cancelAuth.ts
@@ -1,19 +1,21 @@
-import { existsSync, readFileSync, writeFileSync } from 'fs'
-import ini from 'ini'
+import ora from 'ora'
+import { existsSync, unlinkSync } from 'fs'
 import config from '../config'
+import stdout from '../utils/stdout'
+
 
 export default async function () {
-  if (existsSync(config.authPath)) {
-    const conf = ini.parse(readFileSync(config.authPath, 'utf-8'))
-    if (typeof conf.openaiApiKey !== 'undefined' && conf.openaiApiKey) {
-      conf.openaiApiKey = ''
+  try {
+    if (existsSync(config.authPath)) {
+      const spinner = ora({
+        text: stdout.blue('Cancel authorization...')
+      })
+      spinner.start()
+      unlinkSync(config.authPath)
+      spinner.succeed(stdout.green('Cancel authorization successfully'))
     }
-    if (typeof conf.openaiAccessToken !== 'undefined' && conf.openaiAccessToken) {
-      conf.openaiAccessToken = ''
-    }
-    writeFileSync(config.authPath, ini.stringify(conf))
+  } catch (error) {
+    stdout.error(`cancel auth error: ${error}`)
+    process.exit(1)
   }
-
-  console.log(`Cancel authorization successfully`)
-  process.exit(0)
 }
